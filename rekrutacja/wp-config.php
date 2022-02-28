f14ec07cbd11fb3ac739c571c7a0b55652fb103e
Maciej Zaleski <32987640+jadame10@users.noreply.github.com>
2022-02-28 07:39:22 +0100

    Update wp-config.php

--- rekrutacja/wp-config.php
+++ rekrutacja/wp-config.php
@@ -20,7 +20,7 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'cut2code' );
+define( 'DB_NAME', 'c2c' );
 
 /** MySQL database username */
 define( 'DB_USER', 'root' );
